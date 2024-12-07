276dd3af85f3748463c1cb7f8db0c2b4ba5bdc61
Anonym26 <romanfilkov26@gmail.com>
2024-12-07 23:52:44 +0500

    добавлены параметры чтобы не завершало сессии ТГ на всех устройствах

--- test_telegram_API.py
+++ test_telegram_API.py
@@ -4,11 +4,17 @@ import os
 
 # Загрузка API-ключей из файла .env
 load_dotenv()
-TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID")
-TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")
+TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID_2")
+TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH_2")
 
 # Создайте клиент Telegram
-client = TelegramClient('session_name', TELEGRAM_API_ID, TELEGRAM_API_HASH)
+client = TelegramClient('session_name', TELEGRAM_API_ID, TELEGRAM_API_HASH,
+                        system_version='4.16.30-vxCUSTOM',
+                        device_model="iPhone 13 Pro Max",
+                        app_version="8.4",
+                        lang_code="en",
+                        system_lang_code="en-US"
+                        )
 
 async def main():
     # Выводим имя вашего аккаунта
