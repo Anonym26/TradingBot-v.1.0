11e5f79283ca597065304334a54aa8066937f2b2
Anonym26 <romanfilkov26@gmail.com>
2024-12-08 21:30:54 +0500

    добавление тестов для проверки API ByBit и Telegram

--- tests/test_telegram_API.py
+++ tests/test_telegram_API.py
@@ -4,11 +4,11 @@ import os
 
 # Загрузка API-ключей из файла .env
 load_dotenv()
-TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID_2")
-TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH_2")
+TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID")
+TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")
 
 # Создайте клиент Telegram
-client = TelegramClient('session_name', TELEGRAM_API_ID, TELEGRAM_API_HASH,
+client = TelegramClient('../session_name', TELEGRAM_API_ID, TELEGRAM_API_HASH,
                         system_version='4.16.30-vxCUSTOM',
                         device_model="iPhone 13 Pro Max",
                         app_version="8.4",
